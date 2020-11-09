59fb38f3f7ae92b8feb8267e40a3abde6bf30a6d
D4rkT1d3 <40428421+D4rkT1d3@users.noreply.github.com>
2020-11-09 23:43:11 +0200

    Update EngineFactory.h

--- RacingStrategy (copy)/PartsFactory/include/EngineFactory.h
+++ RacingStrategy (copy)/PartsFactory/include/EngineFactory.h
@@ -9,10 +9,23 @@
 #include "../../Parts/include/EnginePart.h"
 
 class EngineFactory: public PartsFactory {
+    /**
+     * @author Jason Maritz u19053292
+     * @brief Superclass for Engine parts factories
+     */
     friend class Engine;
 public:
     EngineFactory();
+    /**
+     *
+     * @param type : type of part to produce
+     * @return the instantiated part
+     */
     virtual Part* produce(std::string type) = 0;
+    /**
+     *
+     * @param newDef : the new part to set as default prototype for the factories
+     */
     void setDefault(Part* newDef);
 protected:
     EnginePart* defaultICE;

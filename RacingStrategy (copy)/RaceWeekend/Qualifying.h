208d02fcd06e6ecf9cfb96188cad32ac038f9947
Chris-18 <72761948+Chris-18@users.noreply.github.com>
2020-11-09 23:29:00 +0200

    added doxygen documentation

--- RacingStrategy (copy)/RaceWeekend/Qualifying.h
+++ RacingStrategy (copy)/RaceWeekend/Qualifying.h
@@ -13,9 +13,14 @@ class Qualifying
 private:
     F1Car* carArray[20];
 public:
+    ///Constructor that create an Qualifying object for the RaceWeekendInterface to use to run a Qualifying session
+    ///@param array An array of type F1Car
     Qualifying(F1Car** array);
+    ///The function that the RaceWeekendInterface object uses to start a Qualifying session.
+    ///@returns A pointer to the F1Car array
     F1Car** StartQualifying();
 private:
+    ///A function to sort the F1Car array based on qualifying time.
     void sortArray();
 };
 

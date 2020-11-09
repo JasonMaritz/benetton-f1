870a476db4eed54f9612fe40f199557c5dacf503
D4rkT1d3 <40428421+D4rkT1d3@users.noreply.github.com>
2020-11-09 23:28:05 +0200

    Update RaceBuilderDir.h

--- RacingStrategy (copy)/F1Car/include/RaceBuilderDir.h
+++ RacingStrategy (copy)/F1Car/include/RaceBuilderDir.h
@@ -11,9 +11,18 @@
 
 class RaceBuilderDir
 {
+    /**
+ * @author Jason Maritz u19053292
+ * @brief Class responsible for creating cars for the race as well as the simulator
+ */
 public:
     RaceBuilderDir();
+    /**
+     * @param parts, List of parts to build the car with
+     * @return a completed F1Car built with F1Builder
+     */
     F1Car* buildCar(std::vector<Part*> parts);
+    /// resets the resulting F1Car
     void resetCar();
 private:
     F1Builder* build;

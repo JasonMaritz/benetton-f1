dbcf631fd3448fd6dc4ef7dc5a688260fd8ce417
D4rkT1d3 <40428421+D4rkT1d3@users.noreply.github.com>
2020-11-09 23:54:46 +0200

    Update TurboCharger.h

--- RacingStrategy (copy)/Parts/include/TurboCharger.h
+++ RacingStrategy (copy)/Parts/include/TurboCharger.h
@@ -9,9 +9,21 @@
 #include "Part.h"
 
 class TurboCharger: public Part {
+    /**
+     * @author Jason Maritz u19053292
+     * @brief The turboCharger part class
+     */
 public:
     TurboCharger(){type="turbocharger";};
+    /**
+     *
+     * @return performance of the part based on the members
+     */
     virtual double getPerformance();
+    /**
+     *
+     * @return Part* cloned from the part it is called from
+     */
     virtual Part* clone();
 };
 

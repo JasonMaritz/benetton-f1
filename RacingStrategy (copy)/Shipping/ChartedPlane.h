96c33b3791c80f7ac5ddbe081ac2f43bd7f1ad8d
nanitight <71830763+nanitight@users.noreply.github.com>
2020-11-09 23:16:50 +0200

    Added Doxygen comments

--- RacingStrategy (copy)/Shipping/ChartedPlane.h
+++ RacingStrategy (copy)/Shipping/ChartedPlane.h
@@ -17,10 +17,15 @@ class ChartedPlane: public TransportMode
 {
 
 	public: 
+		/// @param aDest specifies the destination for the shipment
+		/// @param r pointer to a ContainerRoute 
+		/// @param a speed of the tranportMode
 		ChartedPlane(Location aCurr, ContainerRoute* r,int a = 200);
+		
+		/// @param aDestination the destination for the sipment
+		virtual time_t eta(Location& aDestination);
 
-		virtual time_t eta(Location aDestination);
-
+		/// @todo changes route transportMode for offloading
 		virtual void changeTransportMode(bool );
 };
 

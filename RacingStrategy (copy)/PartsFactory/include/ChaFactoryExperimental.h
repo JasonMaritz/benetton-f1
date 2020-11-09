b34572733635339d06e1852f63c647cd2fbd9031
D4rkT1d3 <40428421+D4rkT1d3@users.noreply.github.com>
2020-11-09 23:40:26 +0200

    Update ChaFactoryExperimental.h

--- RacingStrategy (copy)/PartsFactory/include/ChaFactoryExperimental.h
+++ RacingStrategy (copy)/PartsFactory/include/ChaFactoryExperimental.h
@@ -10,7 +10,16 @@
 #include "ChaFactory.h"
 
 class ChaFactoryExperimental: public ChaFactory {
+    /**
+     * @author Jason Maritz u19053292
+     * @brief Chassis parts factory to create experimental parts
+     */
 public:
+    /**
+     *
+     * @param type : type of part to produce
+     * @return the instantiated part
+     */
     virtual Part* produce(std::string type);
 };
 

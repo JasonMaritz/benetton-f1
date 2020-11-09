5859e5e292c8e9d5d42e4bf8693ec21647c51885
D4rkT1d3 <40428421+D4rkT1d3@users.noreply.github.com>
2020-11-09 23:28:41 +0200

    Update TestBuilderDir.h

--- RacingStrategy (copy)/F1Car/include/TestBuilderDir.h
+++ RacingStrategy (copy)/F1Car/include/TestBuilderDir.h
@@ -11,8 +11,16 @@
 #include "F1Builder.h"
 
 class TestBuilderDir{
+    /**
+ * @author Jason Maritz u19053292
+ * @brief Class responsible for creating cars for testing purposes
+ */
 public:
     TestBuilderDir();
+    /**
+     * @param parts, a Vector of parts to construct the cars from
+     * @return a map of part* and F1Car* that map each car with a specific part to the part it was made with
+     */
     std::map<Part*,F1Car*> buildCars(std::vector<Part*> parts);
 private:
     F1Builder* build;

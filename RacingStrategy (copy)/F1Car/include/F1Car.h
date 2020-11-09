63f6d169cdf4b8513e1efe2b1a928d137a816677
D4rkT1d3 <40428421+D4rkT1d3@users.noreply.github.com>
2020-11-09 23:34:01 +0200

    Update F1Car.h

--- RacingStrategy (copy)/F1Car/include/F1Car.h
+++ RacingStrategy (copy)/F1Car/include/F1Car.h
@@ -19,21 +19,38 @@ class Result;
 
 class F1Car
 {
+    /**
+ * @author Jason Maritz u19053292
+ * @brief F1Car, a container for all the parts with the needed functionality to get the performance thereof
+ */
     friend class F1Builder;
 public:
-    ~F1Car(){if(res) delete res;};
+    ~F1Car();
+    ///@return the average performance of the parts of the car
     double getPerformance();
+    ///@param  result: the result pointer to set res equal to
     void setResult(Result* result){res = result;};
+    ///@brief generates report for simulation
     void generateReport();
+    ///@param name : value we should set driver name to
     void setDriverName(string name);
+    ///@param name : value we should set car name to
     void setCarName(string name);
+    ///@return the name of the driver
     string getDriverName();
+    ///@return the name of the car
     string getCarName();
+    ///@param time : value to set the Qualifying time to
     void setQualifyingTime(double time);
+    ///@param time : value to set the Lap time to
     void setLapTime(double time);
+    ///@param time : value to set the race time to
     void setRaceTime(double time);
+    ///@return returns the qualifying time
     double getQualifyingTime();
+    ///@return returns the lap time
     double getLapTime();
+    ///@return returns the race time
     double getRaceTime();
 private:
     Result* res = nullptr;
